4b65b7a6f02a30b028230c615fd918b6f2392c46
QinzhiChen <qinzhic@gmail.com>
2022-07-12 15:08:38 -0500

    answers inputted

--- case_exercises.sql
+++ case_exercises.sql
@@ -1,5 +1,6 @@
 USE employees;
 
+-- seperate them with 1 or 0
 SELECT
     emp_no,from_date,to_date,
     CASE to_date
@@ -8,13 +9,46 @@ SELECT
     END AS is_current_employee
 FROM dept_emp;
 
+-- Seperate then by alphabetical order
 SELECT first_name,last_name,
 	CASE last_name 
-    WHEN last_name BETWEEN 'A%' AND 'H%' 
+    WHEN last_name >='H%'
     THEN "A-H"
-    WHEN last_name BETWEEN 'I%' AND 'Q%'
+    WHEN last_name >='Q%'
     THEN "I-Q"
-    WHEN last_name BETWEEN 'R%' AND 'Z%'
+    WHEN last_name >='Z%'
+    THEN "R-Z"
+    WHEN last_name="Z%"
     THEN "R-Z"
     END AS "alpha_group"
 FROM employees;
+
+
+-- Seperate them by decade
+SELECT birth_date,
+	CASE
+		WHEN birth_date LIKE "195%" THEN "50S"
+        WHEN birth_date LIKE "196%" THEN "60s"
+	END AS "DECADE"
+FROM employees
+GROUP BY birth_date;
+
+-- Seperate them by department group
+SELECT
+    AVG(salary) average_salary,
+    CASE
+       WHEN dept_name IN ('research', 'development') THEN 'R&D'
+       WHEN dept_name IN ('sales', 'marketing') THEN 'Sales & Marketing'
+       WHEN dept_name IN ('Production', 'Quality Management') THEN 'Prod & QM'
+       WHEN dept_name IN ("Finance", "Human Resources") THEN "Finance&HR"
+       ELSE dept_name
+   END AS dept_group
+FROM salaries
+JOIN dept_emp
+USING (emp_no)
+JOIN departments
+USING (dept_no)
+WHERE dept_emp.to_date>now()
+GROUP BY dept_group;
+
+
